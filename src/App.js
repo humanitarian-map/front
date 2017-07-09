57abf84d2223febb36d326c0a20acaf116fc02e9
Jesús Espino <jespinog@gmail.com>
2017-07-09 12:07:03 +0200

    Adding redux

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react';
 import MapPage from "./pages/MapPage";
 import LoginPage from "./pages/LoginPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import { reducer, initialState} from './App.store';
 import './App.css';
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
+
+let store = createStore(reducer, initialState);
 
 export default class App extends Component {
   render() {
       return (
-		<BrowserRouter>
-          <div className="App">
-            <Route exact path="/"><Redirect to="/login" /></Route>
-            <Route exact path="/map" component={MapPage}/>
-            <Route exact path="/login" component={LoginPage}/>
-            <Route exact path="/profile" component={UserProfilePage}/>
-          </div>
-		</BrowserRouter>
+        <Provider store={store}>
+		  <BrowserRouter>
+            <div className="App">
+              <Route exact path="/"><Redirect to="/login" /></Route>
+              <Route exact path="/map" component={MapPage}/>
+              <Route exact path="/login" component={LoginPage}/>
+              <Route exact path="/profile" component={UserProfilePage}/>
+            </div>
+		  </BrowserRouter>
+        </Provider>
       );
   }
 }
--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import './UserProfilePage.css';
+import Menu from '../components/Menu';
 
-export default class UserProfilePage extends Component {
+class UserProfilePageImpl extends Component {
   render() {
       return (
         <div className="UserProfilePage">
           User Profile
+          <Menu user={this.props.user}/>
         </div>
       );
   }
 }
+
+const UserProfilePage = connect(
+    (state) => ({user: state.user}),
+    (dispatch) => ({
+    })
+)(UserProfilePageImpl);
+
+export default UserProfilePage;
