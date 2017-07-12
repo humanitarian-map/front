06d9346a79bdb744f7f212e879ad69d4d8f41da8
andy <andres.gonzalez@kaleidos.net>
2017-07-12 10:48:22 +0200

    spaces and headings

--- src/components/ProjectDetail.js
+++ src/components/ProjectDetail.js
@@ -7,22 +7,26 @@ import './ProjectDetail.css';
 export default function ProjectDetail(pros) {
     return (
       <section className="ProjectDetail">
-        <h2 className="title">Project</h2>
+        <h2 className="header-title">Project</h2>
         <div className="content">
           <div className="block">
-            <h2 className="name">Project name que se va a dos líneas sin problemas</h2>
+            <h3 className="title project-name">Project name que se va a dos líneas sin problemas</h3>
             <p className="description">Project description que se va seguramente a unas cuantas líneas más a unas cuantas líneas más a unas cuantas líneas más a unas cuantas líneas más a unas cuantas líneas más a unas cuantas líneas más</p>
           </div>
           <div className="block">
-            <h2 className="title mdi mdi-lan mdi-16px">Organization</h2>
+            <h3 className="title mdi mdi-lan mdi-16px">Organization</h3>
+            <div className="organization">
+              <span className="logo"></span>
+              Médicos del Mundo
+            </div>
           </div>
           <div className="block">
-            <h2 className="title mdi mdi-attachment mdi-16px">Documents</h2>
+            <h3 className="title mdi mdi-attachment mdi-16px">Documents</h3>
             <a href="#" alt="">'https://drive.google.com/drive/u/0/folders/0BwVWP_fda2O1fnRDWGNVcFd2N0RfUnBNVENrVnZRTGdTNGNwSExabHFnQmdtVzVPR3VKRU0'</a>
             <a href="#" alt="">'https://drive.google.com/drive/u/0/folders/0BwVWP_fda2O1fnRDWGNVcFd2N0RfUnBNVENrVnZRTGdTNGNwSExabHFnQmdtVzVPR3VKRU0'</a>
           </div>
           <div className="block">
-            <h2 className="title mdi mdi-map-marker mdi-16px">Markers</h2>
+            <h3 className="title mdi mdi-map-marker mdi-16px">Markers</h3>
             <ul>
               <li>
                 <span className="name">Camps</span>
@@ -42,7 +46,7 @@ export default function ProjectDetail(pros) {
             </ul>
           </div>
           <div className="block">
-            <h2 className="title mdi mdi-calendar mdi-16px">Timeframe</h2>
+            <h3 className="title mdi mdi-calendar mdi-16px">Timeframe</h3>
             <div  className="timeframe">
               <span className="label">Start:</span> <span className="value">23/09/2017</span>
               <span className="label">End:</span> <span className="value">01/02/2018</span>
