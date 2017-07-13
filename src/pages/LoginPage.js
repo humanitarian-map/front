6e8f6984ac904edf75dd8bdea1536aa941c00c39
andy <andres.gonzalez@kaleidos.net>
2017-07-13 15:54:48 +0200

    login styles

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import './LoginPage.css';
+import logoSvg from './img/logo-navbar.svg';
 
 export default class LoginPage extends Component {
   render() {
       return (
         <div className="LoginPage">
-          <img className="logo" alt="logo" src="http://www.makeachildcry.com/mobile/assets/img/logos/logo-mdm-ar.svg" />
+          <img className="logo" src={logoSvg} />
+          <h1>The Humanitarian Map</h1>
+          <h2>Visualize your humanitarian project in a map</h2>
           <form>
             <fieldset>
               <input type="text" placeholder="Username" />
