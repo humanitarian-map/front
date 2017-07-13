af2d4d48a3e5457caed483ec1125b3844c4324fc
Jesús Espino <jespinog@gmail.com>
2017-07-13 21:20:21 +0200

    MarkerDetail works when documents integration fails

--- src/components/MarkerDetail.js
+++ src/components/MarkerDetail.js
@@ -25,81 +25,85 @@ function MarkerIcon(props) {
 
 class MarkerDetailImpl extends React.Component {
     render() {
-        let props = this.props
-        let projectDocuments = props.documents.filter((doc) => {
-            let isFile = doc.get('type') === "file";
-            let splittedPath = doc.get('path').split("/");
-            let isInThePointFolder = splittedPath.length >= 4 && splittedPath[2] === props.marker.name;
-            return isFile && isInThePointFolder;
-        });
+        let marker = this.props.marker;
+
+        let projectDocuments = []
+        if (this.props.documents) {
+          projectDocuments = this.props.documents.filter((doc) => {
+              let isFile = doc.get('type') === "file";
+              let splittedPath = doc.get('path').split("/");
+              let isInThePointFolder = splittedPath.length >= 4 && splittedPath[2] === marker.name;
+              return isFile && isInThePointFolder;
+          });
+        }
 
         return (
           <section className="MarkerDetail panel">
             <h2 className="header-title">
-              {props.marker.type === "point" && "Marker"}
-              {props.marker.type === "arrow" && "Arrow"}
-              {props.marker.type === "polygon" && "Polygon"}
-              {props.marker.type === "cross" && "Cross"}
+              {marker.type === "point" && "Marker"}
+              {marker.type === "arrow" && "Arrow"}
+              {marker.type === "polygon" && "Polygon"}
+              {marker.type === "cross" && "Cross"}
             </h2>
             <div className="content">
               <div className="block">
-                <h3 className="title panel-name">{props.marker.name}</h3>
+                <h3 className="title panel-name">{marker.name}</h3>
               </div>
-              {props.marker.type === "point" &&
+              {marker.type === "point" &&
                 <div className="block">
                   <h3 className="title mdi mdi-bookmark mdi-16px">Category</h3>
                   <div className="markers">
-                    <MarkerIcon type={props.marker.data.icon} />
+                    <MarkerIcon type={marker.data.icon} />
                   </div>
                 </div>}
               <div className="block">
                 <h3 className="title mdi mdi-comment mdi-16px">Comment</h3>
-                <p>{props.marker.description}</p>
+                <p>{marker.description}</p>
               </div>
               <div className="block">
                 <h3 className="title mdi mdi-map mdi-16px">Coordinates</h3>
-                {(props.marker.type === "point" || props.marker.type === "cross") &&
+                {(marker.type === "point" || marker.type === "cross") &&
                   <div className="coordinates-inputs">
-                      <span>Latitude {props.marker.data.position[0].toFixed(4)}</span>
-                      <span>Longitude {props.marker.data.position[1].toFixed(4)}</span>
+                      <span>Latitude {marker.data.position[0].toFixed(4)}</span>
+                      <span>Longitude {marker.data.position[1].toFixed(4)}</span>
                   </div>}
-                {props.marker.type === "arrow" &&
+                {marker.type === "arrow" &&
                   <div className="coordinates-inputs">
                       <h4>Origin</h4>
-                      <span>Latitude {props.marker.data.origin[0].toFixed(4)}</span>,
-                      <span>Longitude {props.marker.data.origin[1].toFixed(4)}</span>
+                      <span>Latitude {marker.data.origin[0].toFixed(4)}</span>,
+                      <span>Longitude {marker.data.origin[1].toFixed(4)}</span>
                       <h4>Destination</h4>
-                      <span>Latitude {props.marker.data.dest[0].toFixed(4)}</span>,
-                      <span>Longitude {props.marker.data.dest[1].toFixed(4)}</span>
+                      <span>Latitude {marker.data.dest[0].toFixed(4)}</span>,
+                      <span>Longitude {marker.data.dest[1].toFixed(4)}</span>
                   </div>}
-                {props.marker.type === "polygon" &&
+                {marker.type === "polygon" &&
                   <div className="coordinates-inputs">
-                      {props.marker.data.positions.map((point, idx) => (
+                      {marker.data.positions.map((point, idx) => (
                         <p key={idx}>
                           <span>Latitude {point[0].toFixed(4)}</span>,
                           <span>Longitude {point[1].toFixed(4)}</span>
                         </p>))}
                   </div>}
               </div>
-              {props.marker.type === "point" &&
+              {marker.type === "point" &&
                 <div className="block block-documents">
                   <h3 className="title mdi mdi-attachment mdi-16px">Documents</h3>
                   <div className="docs">
                     {(projectDocuments || []).map((doc) => (
                       <a className="ellipsis"
                          key={doc.get('path')}
-                         href={props.marker.documents_url}
+                         href={marker.documents_url}
                          target="_blank"
                          alt={doc.get('name')}>
                         {doc.get('name')}
                       </a>
                     ))}
-                    <a target="_blank" href={props.marker.documents_url}>Open documents folder</a>
+                    <a target="_blank" href={marker.documents_url}>Open documents folder</a>
                   </div>
                 </div>}
             </div>
             <div className="buttons-set">
-              <button className="delete" onClick={() => props.onDeleteMarker(props.project.get('slug'), props.marker.id)}>Delete</button>
+              <button className="delete" onClick={() => this.props.onDeleteMarker(this.props.project.get('slug'), marker.id)}>Delete</button>
             </div>
           </section>
         );
