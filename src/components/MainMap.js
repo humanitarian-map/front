4b8cce9523eee21b195058c548adb23902e11a47
David Barragán Merino <david.barragan@kaleidos.net>
2017-07-13 18:33:58 +0200

    Change map title

--- src/components/MainMap.js
+++ src/components/MainMap.js
@@ -115,9 +115,13 @@ class MainMapImpl extends React.Component {
             <div className={"cover " + (drawingType? "drawing-"+drawingType : "")}
                  onClick={this.click}></div>}
           <TileLayer
-            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'
+            attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+            subdomains='abcd'
+            maxZoom='19'
+            minZoom='3'
           />
+
           {this.props.project.get('mapitems').map((point) => (
               <Point selected={point.get('id') === this.props.selectedId}
                      onClickItem={this.props.onClickItem}
